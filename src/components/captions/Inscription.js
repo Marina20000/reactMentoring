bcffb691876a8aa8b86b62cef7b738124f53786d
Marina_Voronetckaia <Marina_Voronetckaia@epam.com>
2018-05-05 20:25:09 +0400

    added React markup

---
+++ src/components/captions/Inscription.js
@@ -0,0 +1,10 @@
+import React, {PureComponent} from "react";
+
+export default class Inscription extends PureComponent{
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return <div className={this.props.className}>{this.props.inscription}</div>;
+    }
+}
